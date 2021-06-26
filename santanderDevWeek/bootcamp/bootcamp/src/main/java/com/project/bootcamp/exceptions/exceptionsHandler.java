a25cd38da7d927a675f291ef063d919e485fa7eb
Giuliano Neves Damasceno <giuliano131@hotmail.com>
2021-06-26 02:18:47 -0300

    Atualizando workspace

---
+++ santanderDevWeek/bootcamp/bootcamp/src/main/java/com/project/bootcamp/exceptions/exceptionsHandler.java
@@ -0,0 +1,8 @@
+package com.project.bootcamp.exceptions;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class exceptionHandler extends ResponseEntityExceptionHandler {
+}
